b333a0af7057ddae3cd4eff1d8f8cfb20e5c7208
takao <luciano.takao@gmail.com>
2015-12-10 11:38:22 -0200

    Lib, Exemplo 1 e Exemplo 2.

---
+++ exemplo_01.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once 'lib/Twig/Autoloader.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('template');
+
+
+/* Configuração com uso para cache
+$twig = new Twig_Environment(
+			$loader,
+			array('cache' => 'template/cache')
+		);
+*/
+
+$twig = new Twig_Environment(
+			$loader,
+			array('cache' => false)
+		);
+
+echo $twig->render('base.tpl.php', array('nome' => 'Fulano da Silva'));
---
+++ exemplo_02.php
@@ -0,0 +1,5 @@
+<?php 
+
+require_once 'app.php';
+
+echo $twig->render('base.tpl.php', array('nome' => 'Fulano da Silva'));
